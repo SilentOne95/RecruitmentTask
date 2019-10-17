85c50778285fecc13d73162f955dd73e38e6787e
SilentOne <SilentOne@onet.pl>
2019-10-18 00:12:58 +0200

    Added splash screen

---
+++ app/src/main/java/com/example/recruitmenttask/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.example.recruitmenttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
