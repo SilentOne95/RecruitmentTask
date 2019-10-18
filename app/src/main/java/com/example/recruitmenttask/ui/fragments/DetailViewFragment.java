8b9c3d6355119f0c9825f13716ffeaf7a9751044
SilentOne <SilentOne@onet.pl>
2019-10-18 03:16:13 +0200

    Created nav graph with fragments and anim

---
+++ app/src/main/java/com/example/recruitmenttask/ui/fragments/DetailViewFragment.java
@@ -0,0 +1,36 @@
+package com.example.recruitmenttask.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.recruitmenttask.R;
+
+public class DetailViewFragment extends Fragment {
+
+    private View mView;
+
+    public DetailViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_detail_view, container, false);
+        return mView.getRootView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Details");
+    }
+}
---
+++ app/src/main/java/com/example/recruitmenttask/ui/fragments/ListViewFragment.java
@@ -0,0 +1,36 @@
+package com.example.recruitmenttask.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.recruitmenttask.R;
+
+public class ListViewFragment extends Fragment {
+
+    private View mView;
+
+    public ListViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_list_view, container, false);
+        return mView.getRootView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("List");
+    }
+}
